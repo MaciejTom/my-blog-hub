ee509ffbf225e599b78c0b7d6c1edd779a629486
Maciej Tomaszewicz <1994tomaszewicz@gmail.com>
2023-07-03 16:16:38 +0200

    Basic layout added.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    
+      <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Hello and Welcome 👋&nbsp;
+        <span className="whitespace-nowrap">
+          I&apos;m <span className="font-bold">Maciej</span>.
+        </span>
+      </p>
+      {/* <Posts /> */}
     </main>
-  )
+  );
 }
